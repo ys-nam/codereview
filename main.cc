7e2f980dd8c01ab33271e3c9a46fadba31d825a3
ys-nam <49551253+ys-nam@users.noreply.github.com>
2019-04-17 16:38:18 +0900

    Update main.cc
    코드 리뷰 내용 반영

--- main.cc
+++ main.cc
@@ -23,10 +23,12 @@ int main(int argc, char* argv[]){
 	if(priQueue.empty()){
 		cout << " [!] Priority Queue is empty - initalized " << endl;
 	}
-
-	pushQueue(&priQueue,23);
-	pushQueue(&priQueue,598);
-	pushQueue(&priQueue,302);
+	
+	int test_array[] = {23, 598, 302};
+	int test_count = sizeof(test_array) / sizeof(int);
+	for (int i=0; i<test_count; i++) {
+		pushQueue(&priQueue, test_array[i]);
+	}
 
 	if(argc > 1){
 		customNumber = atoi(argv[1]);
@@ -49,8 +51,10 @@ int main(int argc, char* argv[]){
 		cout << " [+] Priority Queue has data. - Queue Size : " << priQueue.size() <<endl;
 	}
 
-	popQueue(&priQueue);
-	popQueue(&priQueue);
+	test_count = 2;
+	for (int i=0; i<test_count; i++) {
+		popQueue(&priQueue);
+	}
 
 	cout << " [+] Now, Top element of Priority Queue is " << priQueue.top() << endl;
 	
